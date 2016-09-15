303a1d92b3a3c33823a04f955c88ceabb1f56d57
Eric Tam <eric.tam@traceyanderic.com>
2016-09-15 20:54:12 +1000

    removed all TLS test code

--- scraper.rb
+++ scraper.rb
@@ -21,13 +21,7 @@ end
 
 puts "Scraping from " + dateFrom + " to " + dateTo + ", changable via MORPH_PERIOD variable"
 
-agent = Mechanize.new  {|a| a.ssl_version, a.verify_mode = 'TLSv1_2'}
-
-# params = OpenSSL::SSL::SSLContext::DEFAULT_PARAMS
-# params[:ssl_version] = :TLSv1_1
-# #params[:ciphers] = ['DES-CBC3-SHA']
-# OpenSSL::SSL::SSLContext::DEFAULT_PARAMS = params
-
+agent = Mechanize.new
 basepage = agent.get(base_url)
 datepage = basepage.iframes.first.click
 
